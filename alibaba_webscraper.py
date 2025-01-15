51aca69c44dfeb8c9e75e540cabfcb09b9cd3709
Antonio Bravo <antoniodbl10@gmail.com>
2025-01-15 22:10:47 +0000

    Version V1

--- alibaba_webscraper.py
+++ alibaba_webscraper.py
@@ -32,10 +32,27 @@ headers = {
 
 # Hacemos click en la X para evitar el pop-up
 # Esperar hasta que el botón de cierre esté presente
-"""dismiss_button = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.XPATH, "//button[contains(@data-tracking-control-name, 'public_jobs_contextual-sign-in-modal_modal_dismiss')]"))
-)"""
+dismiss_button = WebDriverWait(driver, 5).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[contains(@data-tracking-control-name, 'public_jobs_contextual-sign-in-modal_modal_dismiss')]"))
+)
 
-# Hacer clic en el botón
-#dismiss_button.click()
+
+dismiss_button.click()
+
+# Obtener todos los álbumes disponibles
+albums = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.XPATH, "/div[contains(@class, 'icbu-product-card vertical large product-item')]"))
+)
+
+albums = driver.find_elements(By.XPATH, "//img[contains(@class, 'product-image')]")
+
+if albums:
+    print(len(albums))
+
+# Descargar la imagen usando la sesión
+#mg_data = requests.get(url_imagen, headers=headers).content
+
+
+
+
 
